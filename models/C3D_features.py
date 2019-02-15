0046a692466637390ab24925afca3f7d95d5dfc5
nevinbaiju <nevinbaiju@gmail.com>
2019-02-15 23:00:28 +0530

    added models

---
+++ models/C3D_features.py
@@ -0,0 +1,73 @@
+from C3D_model import C3D
+
+import torch
+import torch.nn as nn
+
+class C3D_CNN(nn.Module):
+	"""
+	The C3D network as described in [1].
+	"""
+
+	def __init__(self, weights="../weights/c3d.pickle"):
+		super(C3D_CNN, self).__init__()
+
+		layers = self.load_layers(weights)
+
+		self.conv1 = layers[0]
+		self.pool1 = layers[1]
+
+		self.conv2 = layers[2]
+		self.pool2 = layers[3]
+
+		self.conv3a = layers[4]
+		self.conv3b = layers[5]
+		self.pool3 = layers[6]
+
+		self.conv4a = layers[7]
+		self.conv4b = layers[8]
+		self.pool4 = layers[9]
+
+		self.conv5a = layers[10]
+		self.conv5b = layers[11]
+		self.pool5 = layers[12]
+		self.fc6 = layers[13]
+
+		self.relu = nn.ReLU()
+
+	def load_layers(self, weights):
+		model = C3D()
+		model.load_state_dict(torch.load(weights))
+		C3D_CNN_LIST = list(model.children())[:-5]
+
+		return C3D_CNN_LIST
+
+
+	def forward(self, x):
+
+		h = self.relu(self.conv1(x))
+		h = self.pool1(h)
+
+		h = self.relu(self.conv2(h))
+		h = self.pool2(h)
+
+		h = self.relu(self.conv3a(h))
+		h = self.relu(self.conv3b(h))
+		h = self.pool3(h)
+
+		h = self.relu(self.conv4a(h))
+		h = self.relu(self.conv4b(h))
+		h = self.pool4(h)
+
+		h = self.relu(self.conv5a(h))
+		h = self.relu(self.conv5b(h))
+		h = self.pool5(h)
+
+		h = h.view(-1, 8192)
+		h = self.relu(self.fc6(h))
+
+		return h
+
+if __name__ == '__main__':
+	model = C3D_CNN()
+	print(model(torch.rand((1, 3, 32, 112, 112))).shape)
+	s = input()
---
+++ models/anomaly_ann.py
@@ -0,0 +1,76 @@
+import torch
+from scipy.io import loadmat
+import torch.nn as nn
+
+def conv_dict(dict2):
+    i = 0
+    dict = {}
+    for i in range(len(dict2)):
+        if str(i) in dict2:
+            if dict2[str(i)].shape == (0, 0):
+                dict[str(i)] = dict2[str(i)]
+            else:
+                weights = dict2[str(i)][0]
+                weights2 = []
+                for weight in weights:
+                    if weight.shape in [(1, x) for x in range(0, 5000)]:
+                        weights2.append(weight[0])
+                    else:
+                        weights2.append(weight)
+                dict[str(i)] = weights2
+    return dict
+
+def get_weight(weight_path):
+    # To load the weights from file
+    dict2 = loadmat(weight_path)
+    weights = conv_dict(dict2)
+    
+    weights['0'][0] = torch.transpose(torch.tensor(weights['0'][0]), 0, -1)
+    weights['0'][1] = torch.tensor(weights['0'][1])
+    print(weights['0'][0].shape)
+    
+    weights['2'][0] = torch.transpose(torch.tensor(weights['2'][0]), 0, -1)
+    weights['2'][1] = torch.tensor(weights['2'][1])
+    print(weights['2'][0].shape)
+        
+
+    weights['4'][0] = torch.transpose(torch.tensor(weights['4'][0]), 0, -1)
+    weights['4'][1] = torch.tensor(weights['4'][1])
+    return weights
+
+class anomaly_ann(nn.Module):
+    def __init__(self, weights_path='../weights/weights_L1L2.mat'):
+        super(anomaly_ann, self).__init__()
+        
+        weights = get_weight(weights_path)
+
+        self.layer1 = nn.Linear(4096, 512)
+        self.layer1.weight.data = weights['0'][0]
+        self.layer1.bias.data = weights['0'][1]
+
+        self.layer2 = nn.Linear(512, 32)
+        self.layer2.weight.data = weights['2'][0]
+        self.layer2.bias.data = weights['2'][1]
+
+        self.layer3 = nn.Linear(32, 1)
+        self.layer3.weight.data = weights['4'][0]
+        self.layer3.bias.data = weights['4'][1]
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        #print("x", x.shape)
+        out = self.layer1(x)
+        out = self.relu(out)
+        
+        out = self.layer2(out)
+
+        out = self.layer3(out)
+
+        return self.sigmoid(out)
+
+if __name__ == '__main__':
+    net = anomaly_ann()
+    print(net)
+    print(net(torch.rand(4096)))
