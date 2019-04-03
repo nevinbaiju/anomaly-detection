78d8a28f6d908be5d4050fe5329c1a44f1ba42d9
nevinbaiju <nevinbaiju@gmail.com>
2019-04-03 19:32:44 +0530

    added video preview with score

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 from models.anomaly_detector import anomaly_detector
 
-#from scripts.db import db
+from scripts.db import db
 from scripts.read_video import generate_block
 from scripts.mail import send_mail
 
@@ -9,13 +9,15 @@ import datetime
 import multiprocessing
 import argparse
 
+import cv2
+
 def process_score(score):
 
     timestamp = time.time()
     ts_str = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
     print(score)
     if(score>base_threshold):
-        #db_access.push((ts_str, score))
+        db_access.push((ts_str, score))
         if(score>alert_threshold):
             emailer = multiprocessing.Process(target=send_mail, \
                                               args=("vision.ai.updates@gmail.com", \
@@ -31,16 +33,26 @@ weights_dict = {'c3d' : 'weights/c3d.pickle', 'ann': 'weights/weights_L1L2.mat'}
 verbose = True
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--vid", type=str, default='SampleVideos/videos/RoadAccidents022_x264.mp4')
+parser.add_argument("--vid", type=str, default='/home/nevin/priyanka.mp4')
 args = parser.parse_args()
 
 filename = args.vid
 
-#db_access = db()
-vid = generate_block(filename, 1)
+db_access = db()
+vid = generate_block(filename, 1, return_frame=True)
 
 detector = anomaly_detector(weights_dict)
 
+cv2.namedWindow("preview")
+font = cv2.FONT_HERSHEY_SIMPLEX
+text_pos = (10, 30)
+
 for i, block in enumerate(vid):
-    score = detector.predict(block)
-    process_score(score)
+    score = detector.predict(block['block'])*100
+    #print(score)
+    cv2.putText(block['preview'], "%d percent"%score, text_pos, font, 1, (255, 255, 255))
+    cv2.imshow('preview', block['preview'])
+    key = cv2.waitKey(20)
+    if(key==27):
+        break
+    #process_score(score)
