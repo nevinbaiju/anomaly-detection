489073380ab3e22bcb2d894dd1272e068644de81
nevinbaiju <nevinbaiju@gmail.com>
2019-04-18 10:59:00 +0530

    fix for converting cuda tensor to numpy

--- scripts/gen_features_resnet.py
+++ scripts/gen_features_resnet.py
@@ -76,7 +76,7 @@ def generate_resnet_features(resnet, filename):
         print("\t\t\t\t\t [{}/{}]".format(i+1, total_length), sep='\r', end='\r')
         features = resnet(curr_block['block'])
         features = (features - features.mean())/(features.max() - features.mean())
-        feature_arr.append(features.detach().numpy())
+        feature_arr.append(features.cpu().detach().numpy())
         del features
     total_time = time.time()-start_time
     features = np.rollaxis(np.array(feature_arr), 1 )[0]
