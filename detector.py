4d2fe542e8fd1dac615786f3f3c42680e89d33d7
nevinbaiju <nevinbaiju@gmail.com>
2019-04-26 23:29:33 +0530

    detector in development with score adjustments

--- detector.py
+++ detector.py
@@ -2,6 +2,7 @@ from models.anomaly_detector import anomaly_detector
 
 #from scripts.db import db
 from scripts.read_video import generate_block
+from misc.min_max import get_min_max
 #from scripts.mail import send_mail
 
 import time
@@ -19,26 +20,36 @@ verbose = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--vid", type=str, default='SampleVideos/videos/RoadAccidents022_x264.mp4')
+parser.add_argument('--no_sigmoid', action='store_true', help='If true, no sigmoid for ann')
 args = parser.parse_args()
 
 filename = args.vid
+no_sigmoid = args.no_sigmoid
 if filename == '0':
     filename = 0
 
 #db_access = db()
 vid = generate_block(filename, 1, return_frame=True)
+min, max = get_min_max(filename)
 
-detector = anomaly_detector(weights_dict)
+detector = anomaly_detector(weights_dict, no_sigmoid)
 
 cv2.namedWindow("preview")
 font = cv2.FONT_HERSHEY_SIMPLEX
 text_pos = (10, 30)
 
 for i, block in enumerate(vid):
-    score = detector.predict(block['block'])*100
+    score = detector.predict(block['block'])
+    score = (score-min)/(max-min)
+    if(score < 0):
+        score = -score
+    elif(score>1):
+        score = 1
+    score = score*100
     print(score)
     preview = block['preview']
     for frame in preview:
+        frame = cv2.resize(frame, (500, 360))
         cv2.putText(frame, "%d percent"%score, text_pos, font, 1, (255, 255, 255))
         cv2.imshow('preview', frame)
         key = cv2.waitKey(20)
--- detector_laptop.py
+++ detector_laptop.py
@@ -1,6 +1,7 @@
 from models.anomaly_ann import anomaly_ann
 import pandas as pd
 import torch
+from misc.min_max import get_min_max
 
 #from scripts.db import db
 from scripts.read_video import generate_block
@@ -19,16 +20,14 @@ def generate_tensor(filename):
     features = pd.read_csv(filename, header=None).drop([0], axis=1).values
     features_tensor = torch.tensor(features, dtype=torch.float)
     return features_tensor
-def get_min_max(filename=''):
-    return -1577.4150390625, 1500
 
 base_threshold = 0.3
 alert_threshold = 0.6
 verbose = True
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--vid_base_path", type=str, default='SampleVideos/videos')
-parser.add_argument("--features_base_path", type=str, default='SampleVideos/features')
+parser.add_argument("--vid_base_path", type=str, default='/media/nevin/01D247D89A1BD8F0/dataset/UCF_Crimes/Videos/Training_Normal_Videos_Anomaly')
+parser.add_argument("--features_base_path", type=str, default='/home/nevin/nevin/datasets/anomaly detection/features/c3d/Training_Normal_Videos_Anomaly')
 parser.add_argument("--vid_file", type=str, default='RoadAccidents022_x264')
 parser.add_argument("--weights_path", type=str, default='weights/weights_L1L2.mat')
 args = parser.parse_args()
@@ -41,6 +40,12 @@ features_base_path = args.features_base_path
 vid_file = args.vid_file
 features_base_path = args.features_base_path
 
+vid_path = os.path.join(vid_base_path, vid_file+'.mp4')
+feature_path = os.path.join(features_base_path, vid_file+'.csv')
+
+assert (os.path.exists(vid_path)), "Video, '{}' does not exist".format(vid_path)
+assert (os.path.exists(feature_path)), "Feature file, '{}' does not exist".format(feature_path)
+
 vid = generate_block(os.path.join(vid_base_path, vid_file+'.mp4'), 1, return_frame=True)
 features = generate_tensor(os.path.join(features_base_path, vid_file+'.csv'))
 weights_path = args.weights_path
@@ -51,11 +56,15 @@ cv2.namedWindow("preview")
 font = cv2.FONT_HERSHEY_SIMPLEX
 text_pos = (10, 30)
 
-min, max = get_min_max()
+min, max = get_min_max(vid_file+'.csv')
 
 for i, block in enumerate(vid):
     score = detector(features[i]).item()
     score = (score-min)/(max-min)
+    if(score < 0):
+        score = -score
+    elif(score>1):
+        score = 1
     score = score*100
     print(score)
     preview = block['preview']
